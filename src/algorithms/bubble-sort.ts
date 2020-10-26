5a85d51ddd6b88aae8e3093fd7fccde2fbdc1e9c
Himesh Ramjee <himeshramjee@users.noreply.github.com>
2020-10-26 18:11:22 +0200

    Initial commit with a bubble sort implementation.

---
+++ src/algorithms/bubble-sort.ts
@@ -0,0 +1,71 @@
+import { HighlightSpanKind } from "typescript";
+
+class BubbleSort {
+  private items: number[];
+  private itemCount: number;
+  
+  public constructor(itemsToSort: number[]) {
+    this.items = itemsToSort;  
+    this.itemCount = itemsToSort.length;
+  }
+
+  public displayItems() {
+    console.log(`Displaying ${this.itemCount} sort items.`);
+    this.items.map(i => {
+      process.stdout.write(i + ", ");
+    });
+    console.log("\n");
+  }
+
+  public sort() {
+    console.log("Snorting items...");
+
+    // [3, 1, 8, 5, 2, 4, 9, 10, 6, 7]
+    for (let outer = this.itemCount -1; outer >= 0; outer--) {
+      for (let inner = 0; inner < outer; inner++) {
+        if (this.items[inner] > this.items[inner + 1]) {
+          this.swapItems(inner, inner + 1);
+        }
+      }
+    }
+
+    console.log("Items are now sorted.\n");
+  }
+
+  private swapItems(x: number, y: number) {
+    const z = this.items[x];
+    this.items[x] = this.items[y];
+    this.items[y] = z;
+  }
+}
+
+// ======================= Main :) ======================= 
+
+console.clear();
+
+const maxItemCount = 50;
+const itemsToSort: number[] = new Array<number>();
+
+// Populate arrary
+for (let i: number = 1; i <= maxItemCount; i++) {
+  itemsToSort.push(i);
+}
+
+// Shuffle array - (apparently using Fisher-Yates Algorithm?)
+for (let i: number = itemsToSort.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * i);
+  const temp = itemsToSort[i];
+  itemsToSort[i] = itemsToSort[j];
+  itemsToSort[j] = temp;
+}
+
+// Sort the array
+const boobleSort: BubbleSort = new BubbleSort(itemsToSort);
+boobleSort.displayItems();
+
+let hrStartTime = process.hrtime();
+boobleSort.sort();
+let hrEndTime = process.hrtime(hrStartTime);
+
+boobleSort.displayItems();
+console.info('Execution time (hr): %ds %dms', hrEndTime[0], hrEndTime[1] / 1000000);
